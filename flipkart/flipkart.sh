bafdbb32e957c5f41cba05984d2709b640944785
sruthi <sruthivishal18@gmail.com>
2019-03-20 11:46:24 +1100

    ssh url

---
+++ flipkart/flipkart.sh
@@ -0,0 +1,11 @@
+amazon is one of the most used online shopping app.
+Flipkart contain electronics,
+clothing,
+griceries,
+home appliances,
+foot wear,
+health and beauty,
+medicines,
+etc..
+It is the best that is used by maximum number of people.
+
---
+++ flipkart/string.sh
@@ -0,0 +1,7 @@
+string-var = "hi , this is sruthi "
+echo ${string-var}
+echo $string-var
+echo ${#string-var}
+echo ${string-var : 7}
+echo ${string-var : 3:8}
+echo ${string-var : (-5)}
